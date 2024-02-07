03b965d4a435a0591851f18e527d927b0770ac55
sbvillalobosm <svillalobosm@uc.cl>
2024-02-07 18:04:11 -0300

    modify yaml

--- ExtraFunctions/WaterDrinkingReminder/app.js
+++ ExtraFunctions/WaterDrinkingReminder/app.js
@@ -1,21 +1,14 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
 const PORT = 8080;
 
-// Crear un servidor HTTP
-const server = http.createServer((req, res) => {
-  // Configurar la respuesta para la ruta principal
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Recuerda tomar agua regularmente.');
-  } else {
-    // Si la ruta no es la principal, responder con un código de estado 404
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Ruta no encontrada');
-  }
+// Ruta principal que muestra el mensaje recordando tomar agua
+app.get('/', (req, res) => {
+    res.send('Recuerda tomar agua regularmente.');
 });
 
-// Escuchar en el puerto especificado
-server.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
+// Inicia el servidor en el puerto especificado
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
